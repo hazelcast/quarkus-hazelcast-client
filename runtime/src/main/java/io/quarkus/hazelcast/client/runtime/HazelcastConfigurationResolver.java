b974efe0051cee6a90b164bfa93af8ec9560b64d
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2020-03-13 10:46:45 +0100

    Add extension

---
+++ runtime/src/main/java/io/quarkus/hazelcast/client/runtime/HazelcastConfigurationResolver.java
@@ -0,0 +1,16 @@
+package io.quarkus.hazelcast.client.runtime;
+
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.impl.clientside.FailoverClientConfigSupport;
+
+/**
+ * @author Grzegorz Piwowarek
+ */
+class HazelcastConfigurationResolver {
+    private final HazelcastConfigurationParser parser = new HazelcastConfigurationParser();
+
+    ClientConfig resolveClientConfig(HazelcastClientConfig properties) {
+        ClientConfig clientConfig = FailoverClientConfigSupport.resolveClientConfig(null);
+        return parser.fromApplicationProperties(properties, clientConfig);
+    }
+}
---
+++ test-framework/src/main/java/io/quarkus/test/hazelcast/HazelcastServerTestResource.java
@@ -0,0 +1,30 @@
+package io.quarkus.test.hazelcast;
+
+import java.util.Collections;
+import java.util.Map;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+
+/**
+ * @author Grzegorz Piwowarek
+ */
+public class HazelcastServerTestResource implements QuarkusTestResourceLifecycleManager {
+
+    private volatile HazelcastInstance member;
+
+    @Override
+    public Map<String, String> start() {
+        member = Hazelcast.newHazelcastInstance();
+        return Collections.emptyMap();
+    }
+
+    @Override
+    public void stop() {
+        if (member != null) {
+            member.shutdown();
+        }
+    }
+}
