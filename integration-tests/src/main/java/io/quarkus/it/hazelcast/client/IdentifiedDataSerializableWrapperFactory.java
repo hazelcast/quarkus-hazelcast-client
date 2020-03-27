db0c87a299f837ce5dcb2208316818f1900e563a
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2020-03-27 08:28:29 +0100

    Extract related constants in tests

--- integration-tests/src/main/java/io/quarkus/it/hazelcast/client/IdentifiedDataSerializableWrapperFactory.java
+++ integration-tests/src/main/java/io/quarkus/it/hazelcast/client/IdentifiedDataSerializableWrapperFactory.java
@@ -3,9 +3,12 @@ package io.quarkus.it.hazelcast.client;
 import com.hazelcast.nio.serialization.IdentifiedDataSerializable;
 
 class IdentifiedDataSerializableWrapperFactory implements com.hazelcast.nio.serialization.DataSerializableFactory {
+
+    static final int FACTORY_ID = 42;
+
     @Override
     public IdentifiedDataSerializable create(int typeId) {
-        if (typeId == 42) {
+        if (typeId == IdentifiedDataSerializableWrapper.CLASS_ID) {
             return new IdentifiedDataSerializableWrapper();
         }
 
--- integration-tests/src/main/java/io/quarkus/it/hazelcast/client/PortableWrapperFactory.java
+++ integration-tests/src/main/java/io/quarkus/it/hazelcast/client/PortableWrapperFactory.java
@@ -4,9 +4,12 @@ import com.hazelcast.nio.serialization.Portable;
 import com.hazelcast.nio.serialization.PortableFactory;
 
 class PortableWrapperFactory implements PortableFactory {
+
+    static final int FACTORY_ID = 1;
+
     @Override
     public Portable create(int classId) {
-        if (PortableWrapper.ID == classId) {
+        if (PortableWrapper.CLASS_ID == classId) {
             return new PortableWrapper();
         } else {
             return null;
